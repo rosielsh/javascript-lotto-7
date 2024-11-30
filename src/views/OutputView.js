dd4b556f3b567cc74e17ea357a6e893b19bfc653
suhwa <tnghk9611@naver.com>
2024-12-01 00:08:58 +0900

    feat(Outputview): 당첨 통계 출력하는 기능 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -16,7 +16,17 @@ class OutputView {
     Console.print(logs.join("\n"));
   }
 
-  static printRankResult() {}
+  static printRankResult(ranks, profit) {
+    Console.print("당첨 통계");
+    Console.print("---");
+
+    Console.print(`3개 일치 (5,000원) - ${ranks.first.getCount()}개`);
+    Console.print(`4개 일치 (50,000원) - ${ranks.second.getCount()}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${ranks.third.getCount()}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${ranks.fourth.getCount()}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${ranks.fifth.getCount()}개`);
+    Console.print(`총 수익률은 ${profit}%입니다.`);
+  }
 
   static #printEmptyLine() {
     Console.print("");
