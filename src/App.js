152794c75e37bb42d8f3095afec95956cc6d82f7
suhwa <tnghk9611@naver.com>
2024-12-01 00:09:14 +0900

    feat(Controller): 전체적인 컨트롤러 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import LottoController from "./controller/LottoController.js";
+import InputView from "./views/InputView.js";
+import OutputView from "./views/OutputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    const views = {
+      inputView: InputView,
+      outputView: OutputView,
+    };
+
+    new LottoController(views).start();
+  }
 }
 
 export default App;
