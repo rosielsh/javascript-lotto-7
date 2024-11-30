d4cc0708963983337cba37371f8d076b06c32796
suhwa <tnghk9611@naver.com>
2024-11-30 23:43:58 +0900

    feat(OutputView): 구매 내역을 출력하는 기능 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -6,7 +6,15 @@ class OutputView {
     this.#printEmptyLine();
   }
 
-  static printPurchaseInfo() {}
+  static printPurchaseInfo(lottoCount, lottos) {
+    Console.print(`${lottoCount}개를 구매했습니다.\n`);
+
+    const logs = lottos.map((lotto) => {
+      return `[${lotto.getNumbers().join(", ")}]`;
+    });
+
+    Console.print(logs.join("\n"));
+  }
 
   static printRankResult() {}
 
